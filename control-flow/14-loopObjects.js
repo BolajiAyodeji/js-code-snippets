a452ac114c9601d712d891eca9734b629a6697fc
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-24 15:38:53 +0100

    added objects loops

---
+++ control-flow/14-loopObjects.js
@@ -0,0 +1,12 @@
+const person = {
+  lastName: ': ' + 'bolaji',
+  firstName: ': ' +'ayodeji',
+  age: ': ' + 19,
+  gender: ': ' + 'male',
+  status: ': ' + 'single',
+  beards: ': ' +'NO',
+  skinColor: ': ' + 'dark'
+}
+
+for (key in person)
+console.log(key, person[key]);
