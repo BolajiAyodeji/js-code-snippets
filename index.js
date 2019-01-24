9b0163a109bdcee12660d22fa94a0c9880d8dcf2
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-24 16:08:11 +0100

    update tenary

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Bolaji Ayodeji')
+const num = [3, 5]
+console.log(num);
+
+let max = num[0] > num[1] ? 'yes':'no';
+console.log(max);
--- operators/tenary.js
+++ operators/tenary.js
@@ -1,3 +1,12 @@
-let points = 200; 
-let type = points > 110?'gold':'silver'; 
-console.log(type); 
+//Check points
+let points = 200;
+let type = points > 110?'gold':'silver';
+console.log(type);
+
+//Checks max number
+const num = [3, 5]
+console.log(num);
+
+let max = num[0] > num[1] ? 'yes':'no';
+console.log(max);
+
