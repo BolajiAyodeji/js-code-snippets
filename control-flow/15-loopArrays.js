15f50bef0730758f6a1a16df9075ab147e7d86b3
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-24 15:52:44 +0100

    update array loops

--- control-flow/15-loopArrays.js
+++ control-flow/15-loopArrays.js
@@ -1,7 +1,9 @@
 //for-in method
 const colors = ['red', 'green', 'blue', 'purple', 'orange', 'white', 'black']
 
-for (index in colors)
-console.log(colors[index]);
+for (let item in colors)
+console.log(colors[item]);
 
 //for-of method (es6 standard!!)
+for (let item of colors)
+console.log(item);
