1bfed826ba29c3d95b8ac1b61269d44def8304c3
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-25 15:05:33 +0100

    count truthy

--- control-flow/6-countTruthy.js
+++ control-flow/6-countTruthy.js
@@ -1,11 +1,18 @@
-const array = [0, null, undefined, '', 2, 3];
-
-console.log(countTruthy(array));
+// count number of Truthy and Falsy values in an Array
+// Returns number of Truthy values in the array
+// List of Falsy values
+// - undefined
+// - null
+// - ''
+// - false
+// - 0
+// - NaN
 
 function countTruthy(array) {
   let count = 0;
   for (let value of array)
     if (value)
-      count++;
-  return count;
+    count++;
+    return count;
 }
+console.log(countTruthy([1, 2, 3]));
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function countTruthy(array) {
+// count number of Truthy and Falsy values in an Array
+// Returns number of Truthy values in the array
+// List of Falsy values
+// - undefined
+// - null
+// - ''
+// - false
+// - 0
+// - NaN
 
+function countTruthy(array) {
+  let count = 0;
+  for (let value of array)
+    if (value)
+    count++;
+    return count;
 }
+console.log(countTruthy([1, 2, 3]));
