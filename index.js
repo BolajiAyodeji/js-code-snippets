f777589ead91c66dacb9b7a48c0b2d6d5c7e3bc3
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-25 14:42:19 +0100

    rm spaces

---
+++ index.js
@@ -0,0 +1,3 @@
+function countTruthy(array) {
+
+}
--- objects/5-showNumbers.js
+++ objects/5-showNumbers.js
@@ -2,7 +2,7 @@
 // Display all number count from 0 > limit and number status
 // e.g 2 "EVEN"
 
-function showNumber (limit) {
+function showNumber(limit) {
   for (let i = 0; i <= limit; i++) {
     const response = (i % 2 === 0) ? 'EVEN' : 'ODD';
     console.log(i, response);
