1b86ac46096f0f5242f0334c4d5d1e118706e503
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-25 16:54:56 +0100

    starrs

--- control-flow/10-stars.js
+++ control-flow/10-stars.js
@@ -1,10 +1,12 @@
-showStars(2);
+// Loop stars per row
+// Increment star +1 per row
 
 function showStars(rows) {
   for (let row = 1; row <= rows; row++) {
     let pattern = '';
-    for (let i = 0; i < row; i++)
-      pattern += '*';
+  for (let i = 0; i < row; i++)
+    pattern += '*';
     console.log(pattern);
   }
 }
+showStars(50);
---
+++ index.js
@@ -0,0 +1,9 @@
+function showStars(rows) {
+  for (let row = 1; row <= rows; row++) {
+    let pattern = '';
+  for (let i = 0; i < row; i++)
+    pattern += '*';
+    console.log(pattern);
+  }
+}
+showStars(50);
