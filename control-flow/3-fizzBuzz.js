2d76155f9320260a3977c5e862c98069faaba9fe
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-25 13:05:56 +0100

    fizzBuzz

--- control-flow/3-fizzBuzz.js
+++ control-flow/3-fizzBuzz.js
@@ -1,11 +1,10 @@
-console.log('Bolaji Ayodeji');
-
 //FizzBuzz
 //If number is divisible by 3 ==> Fizz
 //If number is divisible by 5 ==> Buzz
 //If number is divisible by 3 and 5 ==> FizzBuzz
 //If number is not divisible by 3 or 5 ==> Number
 //If input is not a number ==> Not a number!
+
 
 function fizzBuzz (input) {
   if (typeof input !== 'number')
