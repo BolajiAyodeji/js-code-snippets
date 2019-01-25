b8fa33ca23b110c9c5af24cc2e7616f1185c871d
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-25 16:55:33 +0100

    clear index

--- index.js
+++
@@ -1,9 +0,0 @@
-function showStars(rows) {
-  for (let row = 1; row <= rows; row++) {
-    let pattern = '';
-  for (let i = 0; i < row; i++)
-    pattern += '*';
-    console.log(pattern);
-  }
-}
-showStars(50);
