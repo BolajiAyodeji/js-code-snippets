7ed4cdccc70af164b1068646d7821b9c217d4dcb
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-26 05:50:36 +0100

    basic OOP

---
+++ objects/11-methodsInObjects.js
@@ -0,0 +1,17 @@
+// OOP
+// Object Oriented Programming
+
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  isVisible: true,
+  draw: function() {
+    console.log('draw');
+  }
+}
+circle.draw(); // Here the draw function is a Method in this Object
+
+// If a function is part of an Object, in OOP it is called a Method
