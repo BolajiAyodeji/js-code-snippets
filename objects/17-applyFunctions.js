4530d156cdc771e4b24265cd5f257682476713e5
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-26 16:20:50 +0100

    call and apply methods

---
+++ objects/17-applyFunctions.js
@@ -0,0 +1,10 @@
+let person = {
+  fullName: function(city, country) {
+    return this.firstName + " " + this.lastName + "," + city + "," + country;
+  }
+}
+let person1 = {
+  firstName:"Bolaji",
+  lastName: "Ayodeji",
+}
+person.fullName.apply(person1, ["Kogi", "Nigeria"]);
