2dce03880dbb3098f2f4f94dcdbf71ab8335c15a
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-26 21:08:00 +0100

    object properties

--- objects/18-objectsProperties.js
+++ objects/18-objectsProperties.js
@@ -5,14 +5,18 @@ const circle = {
   }
 };
 
+// Loop through objects
 for (let key in circle)
 console.log(key, circle[key]);
 
+// Loop through using "for of loop" with ".keys method"
 for (let key of Object.keys(circle))
 console.log(key);
 
+// Loop through using "for of loop" with ".entries method"
 for (let key of Object.entries(circle))
 console.log(key);
 
+// Check if object contains some property
 let check = ('radius' in circle) ? 'yes' : 'no';
 console.log(check);
