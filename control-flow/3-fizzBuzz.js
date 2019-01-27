20c2148a23025cc811841b80bdc69352f686f349
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-27 12:16:15 +0100

    fizzBuzz

--- control-flow/3-fizzBuzz.js
+++ control-flow/3-fizzBuzz.js
@@ -5,24 +5,30 @@
 //If number is not divisible by 3 or 5 ==> Number
 //If input is not a number ==> Not a number!
 
-function fizzBuzz (input) {
+let fizzBuzz = function (input) {
   if (typeof input !== 'number')
     return 'Not a number!';
 
-    if ((input % 3 === 0) && (input % 5 === 0))
-      return 'FizzBuzz';
+  if ((input % 3 === 0) && (input % 5 === 0))
+    return 'FizzBuzz';
 
-    if (input % 3 === 0)
+  if (input % 3 === 0)
     return 'Fizz';
 
-    if (input % 5 === 0)
+  if (input % 5 === 0)
     return 'Buzz';
 
-    if (input % 3 !== 0 && input % 5 !== 0)
+  if (input % 3 !== 0 && input % 5 !== 0)
     return input;
 
-    return input;
+  return input;
 }
 
-let output = fizzBuzz(7);
-console.log(output);
+try {
+  let output = fizzBuzz(Hello);
+  console.log(output);
+} catch (e) {
+  if (e instanceof ReferenceError) {
+    console.log("Not a number!")
+  }
+}
--- objects/3-fizzBuzz.js
+++ objects/3-fizzBuzz.js
@@ -1,9 +1,13 @@
-const output = fizzBuzz(5);
-console.log(output);
+//FizzBuzz
+//If number is divisible by 3 ==> Fizz
+//If number is divisible by 5 ==> Buzz
+//If number is divisible by 3 and 5 ==> FizzBuzz
+//If number is not divisible by 3 or 5 ==> Number
+//If input is not a number ==> Not a number!
 
-function fizzBuzz(input) {
+let fizzBuzz = function (input) {
   if (typeof input !== 'number')
-    return NaN;
+    return 'Not a number!';
 
   if ((input % 3 === 0) && (input % 5 === 0))
     return 'FizzBuzz';
@@ -11,8 +15,20 @@ function fizzBuzz(input) {
   if (input % 3 === 0)
     return 'Fizz';
 
-    if (input % 5 === 0)
+  if (input % 5 === 0)
     return 'Buzz';
 
+  if (input % 3 !== 0 && input % 5 !== 0)
+    return input;
+
   return input;
+}
+
+try {
+  let output = fizzBuzz(Hello);
+  console.log(output);
+} catch (e) {
+  if (e instanceof ReferenceError) {
+    console.log("Not a number!")
+  }
 }
