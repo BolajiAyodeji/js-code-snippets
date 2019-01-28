c71df5cbb4343924e035f08493d0f5c789929c0c
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-28 12:22:08 +0100

    functions

--- objects/23-addressObject.js
+++ objects/23-addressObject.js
@@ -9,3 +9,23 @@ function showAddress(address) {
   console.log(key, address[key]);
 }
 showAddress(address);
+
+// Factory function method
+function createAddress(street, city, zipCode) {
+  return {
+  street,
+  city,
+  zipCode
+  };
+}
+const address = createAddress('heaven', 'lokoja', 1100);
+console.log(address);
+
+// Constructor function method
+function CreateAddress2(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
+}
+const address2 = new CreateAddress2('earth', 'lokoja', 1001);
+console.log(address2);
