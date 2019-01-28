1fa8ee0fed5d0cfa2d7f166b8fb85ace8cd314f5
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-28 03:14:56 +0100

    template literals

---
+++ objects/21-templateLiterals.js
@@ -0,0 +1,13 @@
+const name = 'Bolaji';
+const email = 'hi@bolajiayodeji.com';
+const password = 'h3lloworld';
+
+const message =
+`Hello ${name}
+Thank you for signing up on our platform
+
+Here are your login details
+email: ${email}
+password: ${password}`;
+
+console.log(message);
