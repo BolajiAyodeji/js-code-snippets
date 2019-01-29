caf1cc14b2aec7bd22658290c47ada7ae586e47f
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-29 01:31:09 +0100

    clear index

--- index.js
+++
@@ -1,9 +0,0 @@
-let priceRanges = [
-  { label:'$', tooltip:'Inexpensive', minPerPerson:0, maxPerPerson:10 },
-  { label:'$$', tooltip:'Moderate', minPerPerson:11, maxPerPerson:20 },
-  { label:'$$$', tooltip:'Expensive', minPerPerson:21, maxPerPerson:50 },
-]
-
-let restaurants = [
-  { avaragePerson: 5 }
-]
