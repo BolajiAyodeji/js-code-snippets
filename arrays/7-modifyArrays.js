9594f32758e24044a4376a179672ddf1aeb70483
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-31 14:56:25 +0100

    modifying arrays

---
+++ arrays/7-modifyArrays.js
@@ -0,0 +1,11 @@
+const numbers = [4, 5];
+console.log(numbers);
+
+// Add to end
+numbers.push(9, 10, 11);
+
+// Add to start
+numbers.unshift(1, 2, 3)
+
+// Add to middle
+numbers.splice(5, 0, 6, 7, 8);
