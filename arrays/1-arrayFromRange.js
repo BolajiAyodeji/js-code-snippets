144951cb545f9b0de4fe7fb1dbe32c5b3dcc3b43
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-01 17:06:35 +0100

    array from range

--- arrays/1-arrayFromRange.js
+++ arrays/1-arrayFromRange.js
@@ -1,10 +1,8 @@
-const numbers = arrayFromRange(1, 4);
-
-console.log(numbers);
-
-function arrayFromRange(min, max) {
+const arrayFromRange = function arrayFromRange (min, max) {
   const output = [];
   for (let i = min; i <= max; i++)
-    output.push(i);
+  output.push(i);
   return output;
 }
+
+console.log(arrayFromRange(1, 100));
