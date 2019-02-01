0cccf5da69666fa309eff2f6e8e9d96a68de1651
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-01 15:12:39 +0100

    joining arrays

---
+++ arrays/15-joiningArrays.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3];
+const joined = numbers.join(',');
+console.log(joined);
+
+// This method is for strings
+const message = 'this is my first message!'
+const split = message.split(' ');
+console.log(split);
+
+console.log(split.join('-'));
