a386400f9c0ff96bde661d9c4f1700af1677ca5a
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-01 17:13:53 +0100

    includes

--- arrays/2-includes.js
+++ arrays/2-includes.js
@@ -1,10 +1,9 @@
-const numbers = [1, 2, 3, 4];
-
-console.log(includes(numbers, -1));
-
+const array = [1, 3, 4, 6]
 function includes(array, searchElement) {
-  for (let element of array)
-    if (element === searchElement)
-      return true;
+  for(let element of array)
+   if (element === searchElement)
+     return true;
   return false;
 }
+
+console.log(includes(array, 4));
