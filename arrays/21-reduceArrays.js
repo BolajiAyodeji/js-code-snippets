a6f9c7f6f6e8eeaf5baff540ffdd17a01afb6591
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-01 16:57:02 +0100

    reduce arrays

---
+++ arrays/21-reduceArrays.js
@@ -0,0 +1,14 @@
+const numbers = [100, 300, 500, 70];
+
+// let sum = 0;
+// for (let n of numbers) {
+//   sum += n;
+// }
+// console.log(sum);
+
+// reduce method
+const sum =numbers.reduce((accummulator, value) => accummulator + value);
+
+console.log(sum);
+
+
