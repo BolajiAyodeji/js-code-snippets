814fc5cb9b3d36b35a4319d5248b97eb95343157
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-01 15:12:51 +0100

    clear index

--- index.js
+++
@@ -1,6 +0,0 @@
-const url = 'https://bolajiayodeji.com/dt/'
-const postTitle = 'Introduction to Chrome Lighthouse'
-
-const urlSlug = postTitle.toLowerCase().split(' ');
-const postUrl = `${url}` + urlSlug.join('-')
-console.log(postUrl);
