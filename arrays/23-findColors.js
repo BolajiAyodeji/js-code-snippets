1df19b3ceb0f67c0203b7f81e1fa7060eb7268dd
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-06 16:09:43 +0100

    findColor.js

---
+++ arrays/23-findColors.js
@@ -0,0 +1,13 @@
+const data = [
+  {a: 'happy', b: 'robin', c: ['blue','green']},
+  {a: 'tired', b: 'panther', c: ['green','black','orange','blue']},
+  {a: 'sad', b: 'goldfish', c: ['green','red']}
+];
+
+const colors = data.reduce((total, amount) => {
+  amount.c.forEach( color => {
+      total.push(color);
+  })
+  return total;
+}, [])
+colors //['blue','green','green','black','orange','blue','green','red']
