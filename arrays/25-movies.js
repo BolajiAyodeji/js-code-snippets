e5d1184e4dbff1abbfb8342565de9e421faa739f
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-06 19:01:03 +0100

    movies sorting

--- arrays/25-movies.js
+++ arrays/25-movies.js
@@ -1,19 +1,19 @@
 const movies = [
-  {title: 'a', year: 2018, rating: 4.5},
-  {title: 'b', year: 2018, rating: 4.7},
-  {title: 'c', year: 2018, rating: 3},
-  {title: 'd', year: 2017, rating: 4.5}
+  {titile: 'a', year: 2018, rating: 4.5}
+  {titile: 'b', year: 2018, rating: 4.7}
+  {titile: 'c', year: 2018, rating: 3}
+  {titile: 'd', year: 2017, rating: 4.5}
 ]
 
 // All movies in 2018 with rating > 4
 // sort them by their rating
-// decending order
-// pick their title only
+// secending order
+// pick their title
 
 const titles = movies
    .filter(m => m.year === 2018 && m.rating >= 4)
    .sort((a, b) => a.rating - b.rating)
    .reverse()
-   .map(m => m.title)
+   .map(m => m.title);
 
    console.log(titles);
