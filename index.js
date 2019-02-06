c66d0ec80ab2d84a7f646439cd4679f8c4c22d5a
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-06 16:30:04 +0100

    naira to dollar converter

--- index.js
+++
@@ -1,6 +0,0 @@
-const fruitBasket = ['banana', 'cherry', 'orange', 'apple', 'cherry', 'orange', 'apple', 'banana', 'cherry', 'orange', 'fig' ];
-const count = fruitBasket.reduce( (tally, fruit) => {
-  tally[fruit] = (tally[fruit] || 0) + 1 ;
-  return tally;
-} , {})
-console.log(count); // { banana: 2, cherry: 3, orange: 3, apple: 2, fig: 1 }
