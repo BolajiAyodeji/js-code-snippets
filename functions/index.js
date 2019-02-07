2ff8010185133600b16e7e27328a4dd973d52cab
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-07 07:14:52 +0100

    refactor maths function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-import { sumAll } from './math/addition.js'
-import { subtractAll } from './math/subtraction.js'
-import { multiplyAll } from './math/multiplication.js'
-import { divideAll } from './math/division.js'
+//imports the math functions from ./math.js
+// Detailed guide on es6 modules import/export: https://www.sitepoint.com/understanding-es6-modules/
+import { sumAll } from './math.js'
+import { subtractAll } from './math.js'
+import { multiplyAll } from './math.js'
+import { divideAll } from './math.js'
 
-console.log(sumAll(2, 3));
-console.log(subtractAll(2, 3));
-console.log(multiplyAll(2, 3));
-console.log(divideAll(2, 3));
+console.log(sumAll(50, 10));
+console.log(subtractAll(50, 10));
+console.log(multiplyAll(50, 10));
+console.log(divideAll(50, 10));
---
+++ functions/math.js
@@ -0,0 +1,15 @@
+export const sumAll = (a, b) => {
+  return a + b;
+}
+
+export const subtractAll = (a, b) => {
+  return a - b;
+}
+
+export const multiplyAll = (a, b) =>{
+  return a * b;
+}
+
+export const divideAll = (a, b) =>{
+  return a / b;
+}
