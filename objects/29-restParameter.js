4293267667ae49b1dce7118f7055716019ca3a55
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-08 00:36:08 +0100

    rest parameter vs arguments keyword

---
+++ objects/29-restParameter.js
@@ -0,0 +1,24 @@
+// rest parameter vs arguments keyword
+
+function Store() {
+  let aisle = {
+    fruit: [],
+    vegetalbe: []
+  }
+  return {
+    //Store().add('category', 'item1', 'item2');
+    add: function(category, ...items) {
+      //let items = [].splice.call(arguments, 1);
+      console.log(items);
+      items.forEach(function(value, index, array) {
+        aisle[category].push(value);
+      });
+    },
+    aisle: aisle
+  }
+}
+
+let myGroceryStore = new Store();
+
+myGroceryStore.add('fruit', 'apples', 'oranges');
+console.log(myGroceryStore.aisle);
