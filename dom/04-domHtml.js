50f49b59750d78e3d05b83860e111ce4d72bab3b
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-08 14:43:06 +0100

    HTML DOM

---
+++ dom/04-domHtml.js
@@ -0,0 +1,4 @@
+let d = new Date();
+document.getElementById('demo').innerHTML= d.getFullYear();
+document.getElementById('img').src = 'https://res.cloudinary.com/iambeejayayo/image/upload/v1547954566/fav-500.png';
+document.getElementById('img').style.width="70%"
