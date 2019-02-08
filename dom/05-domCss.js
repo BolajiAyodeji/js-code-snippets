3b8687755e470fac641702147015fa48b1031da7
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-08 14:56:36 +0100

    DOM CSS

---
+++ dom/05-domCss.js
@@ -0,0 +1,7 @@
+document.getElementById('head').style.color="red";
+document.getElementsByClassName('head2')[0].style.color="green";
+document.getElementsByTagName('h1')[2].style.color="blue";
+
+let changeColor = () => {
+  document.getElementById('head').style.color = 'red';
+}
