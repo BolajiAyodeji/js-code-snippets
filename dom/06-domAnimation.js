868c7bc6e63658f0be6d8bed761d68a68310f390
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-08 15:09:46 +0100

    DOM animation

---
+++ dom/06-domAnimation.js
@@ -0,0 +1,14 @@
+function moveObj() {
+  let elem = document.getElementById("anime");
+  let pos = 0;
+  let id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
