60e873f1f84305a72dc3614f6bfbf72082ea9336
Umar Mash <mafulun4real@gmail.com>
2019-02-09 19:27:44 +0100

    Added a simple way to convert between strings and integer

---
+++ operators/conversion.js
@@ -0,0 +1,9 @@
+// Converting Integer to String
+let age = 34;
+let age_toString = '' + age;
+console.log(age_toString);
+
+// Converting String to Integer
+let length = '785';
+let length_toInt = +length;
+console.log(length_toInt);
