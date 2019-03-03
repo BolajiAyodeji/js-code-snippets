34d1e2e20c612f59110a380a60992aa77a16993e
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-03 21:44:56 +0100

    error handling :zap:

--- functions/03-errorHandling.js
+++ functions/03-errorHandling.js
@@ -1,18 +1,19 @@
+
+function countOccurences(array, searchElement) {
+  if (!Array.isArray(array)) {
+    throw new Error('Enter a valid array');
+  }
+  return array.reduce((accumulator, current) => {
+    const occurence = (current === searchElement) ? 1 : 0;
+    return accumulator + occurence;
+  }, 0)
+}
+
 try {
-  const numbers = [1, 2, 3, 4];
-  const count = countOccurrences(null, 1);
+  const numbers = null;
+  const count = countOccurences(numbers, 1);
   console.log(count);
 }
-catch (e) {
-  console.log(e.message);
-}
-
-function countOccurrences(array, searchElement) {
-  if (!Array.isArray(array))
-    throw new Error('Invalid array.');
-
-  return array.reduce((accumulator, current) => {
-    const occurrence = (current === searchElement) ? 1 : 0;
-    return accumulator + occurrence;
-  }, 0);
+catch(e) {
+  console.log(e.message)
 }
