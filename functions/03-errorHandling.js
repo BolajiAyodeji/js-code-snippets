74ace24d742b7b1a23daab6a4818abf3bc4e9117
eronmmer <eronmmer@gmail.com>
2019-04-05 11:01:53 +0100

    fix typo

--- functions/03-errorHandling.js
+++ functions/03-errorHandling.js
@@ -1,17 +1,17 @@
 
-function countOccurences(array, searchElement) {
+function countOccurrences(array, searchElement) {
   if (!Array.isArray(array)) {
     throw new Error('Enter a valid array');
   }
   return array.reduce((accumulator, current) => {
-    const occurence = (current === searchElement) ? 1 : 0;
-    return accumulator + occurence;
+    const occurrence = (current === searchElement) ? 1 : 0;
+    return accumulator + occurrence;
   }, 0)
 }
 
 try {
   const numbers = null;
-  const count = countOccurences(numbers, 1);
+  const count = countOccurrences(numbers, 1);
   console.log(count);
 }
 catch(e) {
